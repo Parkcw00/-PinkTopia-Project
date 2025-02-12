3fb95fbb1b0fda2b8a8d9fe62f3be5f554caea66
오혜성 <ohs020105@gmail.com>
2025-02-12 21:29:24 +0900

    update : app.module.ts 에 storeItemModule이 추가가 안되어있어서 추가했습니다.

--- src/store-item/store-item.module.ts
+++ src/store-item/store-item.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { StoreItemService } from './store-item.service';
 import { StoreItemController } from './store-item.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { StoreItem } from './entities/store-item.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([StoreItem])],
   controllers: [StoreItemController],
   providers: [StoreItemService],
 })
