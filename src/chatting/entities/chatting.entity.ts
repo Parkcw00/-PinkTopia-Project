04620bc814f05c4c19905c5727a201a174b27518
Parkcw00 <parkcw0325@gmail.com>
2025-02-12 14:04:01 +0900

    update: 채팅 엔티티 작성

--- src/chatting/entities/chatting.entity.ts
+++ src/chatting/entities/chatting.entity.ts
@@ -1,1 +1,23 @@
-export class Chatting {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity({
+  name: 'chatting',
+})
+export class Chatting {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  message: string;
+
+  @Column()
+  image: string;
+
+  @Column()
+  created_at: Date;
+
+  @Column()
+  deleted_at: Date;
+
+  //관계 설정 해야하는 부분! 채팅룸 id 유저 id 받아오기!
+}
