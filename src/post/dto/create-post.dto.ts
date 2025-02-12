6a66c3a60a6dcceee2151267858294490f5224b4
lgm-7 <xlzlrud@naver.com>
2025-02-12 17:59:12 +0900

    update 포스트스웨거추가

--- src/post/dto/create-post.dto.ts
+++ src/post/dto/create-post.dto.ts
@@ -1,11 +1,14 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString } from 'class-validator';
 
 export class CreatePostDto {
   @IsString()
   @IsNotEmpty()
+  @ApiProperty({ example: '게시글 제목' })
   title: string;
 
   @IsString()
   @IsNotEmpty()
+  @ApiProperty({ example: '게시글 내용' })
   content: string;
 }
--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -10,31 +10,37 @@ import {
 import { PostService } from './post.service';
 import { CreatePostDto } from './dto/create-post.dto';
 import { UpdatePostDto } from './dto/update-post.dto';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('포스트CRUD')
 @Controller('post')
 export class PostController {
   constructor(private readonly postService: PostService) {}
-
+  @ApiOperation({ summary: '게시글 생성' })
   @Post()
   createPost(@Body() createPostDto: CreatePostDto) {
     return this.postService.create(1, createPostDto);
   }
 
+  @ApiOperation({ summary: '게시글들 조회' })
   @Get()
   findPosts() {
     return this.postService.findPosts();
   }
 
+  @ApiOperation({ summary: '게시글 조회' })
   @Get(':id')
   findPost(@Param('id') id: string) {
     return this.postService.findPost(+id);
   }
 
+  @ApiOperation({ summary: '게시글 수정' })
   @Patch(':id')
   update(@Param('id') id: string, @Body() updatePostDto: UpdatePostDto) {
     return this.postService.updatePost(1, +id, updatePostDto);
   }
 
+  @ApiOperation({ summary: '게시글 삭제' })
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.postService.deletePost(1, +id);
