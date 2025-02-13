4f8d5405f6c349b27295cd79dcad26b1f34095b6
Parkcw00 <parkcw0325@gmail.com>
2025-02-13 12:30:15 +0900

    update: 모듈 임토프 추가

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -4,9 +4,11 @@ import { UserController } from './user.controller';
 import { UserService } from './user.service';
 import { UserRepository } from './user.repository';
 import { User } from './entities/user.entity';
+import { Chatting } from 'src/chatting/entities/chatting.entity';
+import { AchievementC } from 'src/achievement-c/entities/achievement-c.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User, Chatting, AchievementC])],
   controllers: [UserController],
   providers: [UserService, UserRepository],
 })
