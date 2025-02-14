c697dc9915ea0d3c26e301efb7ae84471c056300
김지웅 <sizera1004@gmail.com>
2025-02-14 20:01:18 +0900

    fix: user guard 코드 정렬

--- src/user/guards/user-guard.ts
+++ src/user/guards/user-guard.ts
@@ -31,49 +31,49 @@ export class UserGuard implements CanActivate {
     const refreshTokenKey = this.configService.get<string>(
       'REFRESH_TOKEN_SECRET_KEY',
     );
-      // 둘 다 존재하지 않는 경우
-      if (!accessToken && !refreshToken) {
-        throw new UnauthorizedException('로그인을 진행해 주세요');
-      }
+    // 둘 다 존재하지 않는 경우
+    if (!accessToken && !refreshToken) {
+      throw new UnauthorizedException('로그인을 진행해 주세요');
+    }
 
-      // access token 만료기간 검증
-      if (accessToken) {
-        const decoded = this.jwtService.verify(accessToken, {
-          secret: accessTokenKey,
-        });
-        if (decoded.exp < currentTime) {
-          accessToken = null;
-        }
-        const payload = { id: decoded.id, email: decoded.email };
-        const newAccessToken = await this.makeAccessToken(payload);
-        accessToken = newAccessToken;
-      }
-
-      // access token만 존재하지 않는 경우, refresh token 존재
-      if (!accessToken && refreshToken) {
-        const decoded = this.jwtService.verify(refreshToken, {
-          secret: refreshTokenKey,
-        });
-        if (decoded.exp < currentTime) {
-          response.clearCookie('refreshToken');
-          throw new Error('다시 로그인 해주세요.');
-        } // refresh token 만료되었을 때 재로그인 유도
-
-        const payload = { id: decoded.id, email: decoded.email };
-        const newAccessToken = await this.makeAccessToken(payload);
-        accessToken = newAccessToken;
-      }
-
-      // access token 검증
+    // access token 만료기간 검증
+    if (accessToken) {
       const decoded = this.jwtService.verify(accessToken, {
         secret: accessTokenKey,
       });
-      let existuser = await this.userRepository.findEmail(decoded.email);
-      if (!existuser) {
-        throw new BadRequestException('인증되지 않은 사용자 입니다.');
+      if (decoded.exp < currentTime) {
+        accessToken = null;
       }
-      try {
-      reqest.user  = {
+      const payload = { id: decoded.id, email: decoded.email };
+      const newAccessToken = await this.makeAccessToken(payload);
+      accessToken = newAccessToken;
+    }
+
+    // access token만 존재하지 않는 경우, refresh token 존재
+    if (!accessToken && refreshToken) {
+      const decoded = this.jwtService.verify(refreshToken, {
+        secret: refreshTokenKey,
+      });
+      if (decoded.exp < currentTime) {
+        response.clearCookie('refreshToken');
+        throw new Error('다시 로그인 해주세요.');
+      } // refresh token 만료되었을 때 재로그인 유도
+
+      const payload = { id: decoded.id, email: decoded.email };
+      const newAccessToken = await this.makeAccessToken(payload);
+      accessToken = newAccessToken;
+    }
+
+    // access token 검증
+    const decoded = this.jwtService.verify(accessToken, {
+      secret: accessTokenKey,
+    });
+    let existuser = await this.userRepository.findEmail(decoded.email);
+    if (!existuser) {
+      throw new BadRequestException('인증되지 않은 사용자 입니다.');
+    }
+    try {
+      reqest.user = {
         id: decoded.id, // JWT 생성 시 sub에 user.id 저장
         email: decoded.email, // 이메일 정보도 저장
       };
@@ -81,9 +81,7 @@ export class UserGuard implements CanActivate {
         response.setHeader('Authorization', `Bearer ${accessToken}`);
       }
     } catch (err) {
-      throw new InternalServerErrorException(
-        '오류가 발생하였습니다.',
-      );
+      throw new InternalServerErrorException('오류가 발생하였습니다.');
     }
 
     return true; // 요청 허용
