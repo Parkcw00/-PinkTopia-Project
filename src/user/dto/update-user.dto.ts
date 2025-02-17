14db9841cc05b8c1c7e39e8bb4cd30ef0b583dd7
김지웅 <sizera1004@gmail.com>
2025-02-17 10:05:16 +0900

    add:기본적인 user crud추가

--- src/user/dto/update-user.dto.ts
+++ src/user/dto/update-user.dto.ts
@@ -1,4 +1,20 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateUserDto } from './create-user.dto';
+import { Type } from "class-transformer";
+import { IsDate, IsOptional, IsString, Length } from "class-validator";
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto {
+  @IsOptional()
+  @IsString({ message: '닉네임은 문자열이어야 합니다.' })
+  nickname?: string;
+
+  @IsOptional()
+  @Length(6, 20, { message: '비밀번호는 6자 이상 20자 이하로 입력해주세요.' })
+  password?: string;
+
+  @IsOptional()
+  profile_image?: string;
+
+  @IsOptional()
+  @Type(() => Date)
+  @IsDate({ message: '생일 형식이 올바르지 않습니다.' })
+  birthday?: Date;
+}
