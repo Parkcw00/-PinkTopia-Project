a3e38b9d4beceb02b0b9e7f8ca0b2196fbbc117f
강나연 <skdusdl4@naver.com>
2025-02-17 21:05:42 +0900

    update / 핑크몽 dto 추가

--- src/pinkmong/dto/create-pinkmong.dto.ts
+++ src/pinkmong/dto/create-pinkmong.dto.ts
@@ -1,1 +1,25 @@
-export class CreatePinkmongDto {}
+import { IsString, IsOptional, IsEnum, IsInt, MaxLength } from 'class-validator';
+
+export class CreatePinkmongDto {
+  @IsString()
+  @MaxLength(255)
+  name: string;
+
+  @IsOptional()
+  @IsString()
+  @MaxLength(255)
+  location_url?: string;
+
+  @IsString()
+  explain: string;
+
+  @IsEnum(['forest', 'desert', 'ocean', 'mountain', 'city'])
+  region_theme: string;
+
+  @IsEnum(['common', 'rare', 'epic', 'legendary'])
+  grade: string;
+
+  @IsInt()
+  point: number;
+}
+
--- src/pinkmong/dto/update-pinkmong.dto.ts
+++ src/pinkmong/dto/update-pinkmong.dto.ts
@@ -1,4 +1,29 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreatePinkmongDto } from './create-pinkmong.dto';
+import { IsString, IsOptional, IsEnum, IsInt, MaxLength } from 'class-validator';
 
-export class UpdatePinkmongDto extends PartialType(CreatePinkmongDto) {}
+export class UpdatePinkmongDto {
+  @IsOptional()
+  @IsString()
+  @MaxLength(255)
+  name?: string;
+
+  @IsOptional()
+  @IsString()
+  @MaxLength(255)
+  location_url?: string;
+
+  @IsOptional()
+  @IsString()
+  explain?: string;
+
+  @IsOptional()
+  @IsEnum(['forest', 'desert', 'ocean', 'mountain', 'city'])
+  region_theme?: string;
+
+  @IsOptional()
+  @IsEnum(['common', 'rare', 'epic', 'legendary'])
+  grade?: string;
+
+  @IsOptional()
+  @IsInt()
+  point?: number;
+}
