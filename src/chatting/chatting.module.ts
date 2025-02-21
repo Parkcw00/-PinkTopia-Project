1ed927a36c46e1044bce3ad375b08d1a9d98b616
lgm-7 <xlzlrud@naver.com>
2025-02-21 17:41:43 +0900

    update 모듈에 필요없는부분 제거

--- src/chatting/chatting.module.ts
+++ src/chatting/chatting.module.ts
@@ -6,13 +6,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Chatting } from './entities/chatting.entity';
 import { Chatmember } from 'src/chatmember/entities/chatmember.entity';
 import { S3Service } from 'src/s3/s3.service';
-import { ValkeyService } from 'src/valkey/valkey.service';
 import { ValkeyModule } from 'src/valkey/valkey.module';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Chatting, Chatmember]), ValkeyModule],
   controllers: [ChattingController],
-  providers: [ChattingService, ChattingRepository, S3Service, ValkeyService],
+  providers: [ChattingService, ChattingRepository, S3Service],
   exports: [ChattingService, ChattingRepository],
 })
 export class ChattingModule {}
