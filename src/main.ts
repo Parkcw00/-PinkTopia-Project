3fccd41055ae112fe58451c196c7e11ba310e0e9
김지웅 <sizera1004@gmail.com>
2025-02-24 11:18:50 +0900

    로그인/회원가입

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,23 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
-import * as cookieParser from 'cookie-parser'
+import * as cookieParser from 'cookie-parser';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  
+  // CORS 설정
+  app.enableCors({
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
+
+  // 정적 파일 제공 설정
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+  
   app.use(cookieParser());
   const options = new DocumentBuilder()
     .setTitle('Your API Title')
@@ -32,6 +45,8 @@ async function bootstrap() {
       stopAtFirstError: true,
     }),
   );
+  
   await app.listen(process.env.PORT ?? 3000);
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
